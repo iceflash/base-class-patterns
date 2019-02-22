90c8aff451dfc5106ec66b1aa7978ba1e307c86d
iceflash <iceflasheroid@gmail.com>
2019-02-22 14:39:05 +0700

    fix: some typos and tests

--- test/Singleton-test.js
+++ test/Singleton-test.js
@@ -2,6 +2,9 @@ import test from 'ava';
 import {Singleton} from '../main';
 import Child from './childClass';
 
+const subclass = class subclass extends Singleton {
+};
+
 test('Should not created directly by constructor', t => {
   const res = t.throws(() => { new Singleton() });
   t.is(res.message, 'This is singleton. Use getInstance()', 'message');
@@ -12,12 +15,18 @@ test('Base method getInstance() must throw exception', t => {
 });
 
 test('Base method getInstance() should be overrided in subclass', t => {
-  const subclass = class subclass extends Singleton {
-
-  };
   t.throws(() => { subclass.getInstance()});
 });
 
 test('Get Instance of Child class Singleton', t => {
-  t.notThrows(() => { const singletonInstance = Child.getInstance(); }, 'message');
+  const res = t.notThrows(() => { 
+    const inst = Child.getInstance();
+    inst.inc(); // counter is 1
+    t.is(inst.counter, 1, 'Counter must be 1');
+  });
+
+  // create other instance
+  const inst2 = Child.getInstance();
+  inst2.inc(); // counter is 2
+  t.is(inst2.counter, 2, 'Counter must be 2');
 });
